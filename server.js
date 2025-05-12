1ca848d34d66f351b07db35074a14bee78af5090
yogaardiansyah <yogaardiansyah@protonmail.com>
2025-05-12 13:26:22 +0700

    feat: register and login route

--- server.js
+++ server.js
@@ -1,22 +1,29 @@
 'use strict';
 
+require('dotenv').config();
 const Hapi = require('@hapi/hapi');
+const AuthRoutes = require('./routes/auth');
 
 const init = async () => {
+  const server = Hapi.server({
+    port: 3000,
+    host: 'localhost',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
 
-    const server = Hapi.server({
-        port: 3000,
-        host: 'localhost'
-    });
+  await server.register(AuthRoutes);
 
-    await server.start();
-    console.log('Server running on %s', server.info.uri);
+  await server.start();
+  console.log('✅ Server running on %s', server.info.uri);
 };
 
 process.on('unhandledRejection', (err) => {
-
-    console.log(err);
-    process.exit(1);
+  console.error(err);
+  process.exit(1);
 });
 
 init();
